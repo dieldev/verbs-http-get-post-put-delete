e4fe1a5141a7a42faee16be03fb12515ac88999b
Sidiel Oliveira <sidiel.dev@gmail.com>
2023-10-21 14:26:18 -0300

    First Steps in Java with Spring Boot 3

---
+++ verbs-http-get-post-put-delete/src/main/java/br/example/diel/VerbsHttpGetPostPutDeleteApplication.java
@@ -0,0 +1,13 @@
+package br.example.diel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbsHttpGetPostPutDeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbsHttpGetPostPutDeleteApplication.class, args);
+	}
+
+}
---
+++ verbs-http-get-post-put-delete/src/test/java/br/example/diel/VerbsHttpGetPostPutDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.example.diel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbsHttpGetPostPutDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
